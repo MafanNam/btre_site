87235a67aa7f436bf1c22756c11e619ef082481f
Maxim Manzik <kepkpkep@gmail.com>
2023-05-09 20:31:25 +0300

    I created bd (listing, realtor), and rename master branch to main.

--- btre_site/settings.py
+++ btre_site/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'btre_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
     }
 }
 
